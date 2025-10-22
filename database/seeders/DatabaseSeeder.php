7b353e2f94d9b7ee65fac0e7265970c768e94f14
Jarbas Santos <jarbas.santos.0409@gmail.com>
2025-10-21 23:58:32 -0300

    chore(database): update default test users with manager credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Gestor 1',
+            'email' => 'manager1@convenia.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Gestor 2',
+            'email' => 'manager2@convenia.com',
+            'password' => bcrypt('123456'),
         ]);
     }
 }
