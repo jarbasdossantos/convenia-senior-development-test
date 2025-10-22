8cd845b22ace552a7aa4cffb89b904527ca6f3b1
Jarbas Santos <jarbas.santos.0409@gmail.com>
2025-10-22 01:28:02 -0300

    refactor(ProcessCollaboratorsCsvJob): improve queue job implementation

--- app/Jobs/ProcessCollaboratorsCsvJob.php
+++ app/Jobs/ProcessCollaboratorsCsvJob.php
@@ -5,21 +5,31 @@ namespace App\Jobs;
 use App\Mail\ProcessCollaboratorsCsvJobNotification;
 use App\Models\Collaborator;
 use App\Models\User;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use League\Csv\Reader;
 
 class ProcessCollaboratorsCsvJob implements ShouldQueue
 {
+    use Dispatchable;
+    use InteractsWithQueue;
     use Queueable;
+    use SerializesModels;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct(protected string $path, protected int $userId)
     {
+    }
+
+    public function handle(): void
+    {
+        Log::info('Processing CSV file', ['path' => $this->path, 'user_id' => $this->userId]);
+
         $user = User::findOrFail($this->userId);
         $fullPath = Storage::path($this->path);
 
@@ -31,36 +41,31 @@ class ProcessCollaboratorsCsvJob implements ShouldQueue
 
         foreach ($reader as $i => $row) {
             try {
-                $collaborator = Collaborator::firstOrCreate([
+                Collaborator::firstOrCreate(
+                    [
                     'cpf' => $row['cpf'],
                     'email' => $row['email'],
-                ], [
-                    'user_id' => $this->userId,
-                    'name' => $row['name'],
-                    'email' => $row['email'],
-                    'city' => $row['city'],
-                    'state' => $row['state'],
-                ]);
-
-                $processed++;
+                    ],
+                    [
+                        'user_id' => $this->userId,
+                        'name' => $row['name'],
+                        'email' => $row['email'],
+                        'city' => $row['city'],
+                        'state' => $row['state'],
+                    ]
+                );
             } catch (\Exception $e) {
                 $errors[] = [
                     'line' => $i + 1,
                     'message' => $e->getMessage(),
                 ];
 
+                Log::error('Error processing line ' . ($i + 1), ['message' => $e->getMessage()]);
+            } finally {
                 $processed++;
             }
         }
 
         Mail::to($user->email)->queue(new ProcessCollaboratorsCsvJobNotification($processed, $errors));
-    }
-
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
-    {
-        //
     }
 }
